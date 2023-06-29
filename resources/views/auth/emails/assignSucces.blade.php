df2353f67f2a4e1f5f942dcecce03c5e824e9901
xuanquocc <quocntx.21it@vku.udn.vn>
2023-06-29 19:57:41 +0700

    edit email assign

--- resources/views/auth/emails/assignSucces.blade.php
+++ resources/views/auth/emails/assignSucces.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div>
         <h2>おはようございます。 {{$creator_name}}</h2>
-        <p>管理者があなたをプロジェクト <span>{{$project_name}}</span> に追加しました</p>
+        <p>管理者があなたをプロジェクト <h3>{{$project_name}}</h3> に追加しました</p>
         <p>しっかり仕事をしてください</p>
     </div>
 </div>
